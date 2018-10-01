3be636ae8f8fc6fe21074b1f09286efd36f624ce
Alissa Balge <alissa.balge@gmail.com>
2018-10-01 23:41:37 +0000

    linting

--- download_avatars.js
+++ download_avatars.js
@@ -1,8 +1,8 @@
 var request = require("request");
 var token = require("./secrets.js");
 var fs = require('fs');
-var repoOwner = process.argv[2]
-var repoName = process.argv[3]
+var repoOwner = process.argv[2];
+var repoName = process.argv[3];
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -21,43 +21,34 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
   request(options, function(err, res, body) {
     var data = JSON.parse(body);
-     cb(err, data);
-
-  });
-
-}
+    cb(err, data);
+    });
+  }
 }
 
 
 function downloadImageByURL(url, filePath) {
   request.get(url)
     .on("error", function (err) {
-    throw err;
+      throw err;
     })
     .on("response", function (response) {
       console.log('Downloading image...');
     })
-
-
      .on("end", function () {
-      console.log('Download complete.');
-
-  })
-
+        console.log('Download complete.');
+    })
     .pipe(fs.createWriteStream(filePath))
-
 }
 
 
 
 getRepoContributors(repoOwner, repoName, function(err, result) {
   console.log("Errors:", err);
-  //console.log("Result:", result);
   for (var i = 0; i < result.length; i++) {
     console.log("Avatar URL: ", result[i].avatar_url);
-    downloadImageByURL(result[i].avatar_url, __dirname + "/avatar/" + result[i].login + ".jpeg")
+    downloadImageByURL(result[i].avatar_url, __dirname + "/avatar/" + result[i].login + ".jpeg");
   }
-
 });
 
 
